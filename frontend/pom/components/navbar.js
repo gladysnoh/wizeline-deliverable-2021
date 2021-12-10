ee93222d608f9cf211728a6cf82c4315ffc66e0b
Gladys Noh <gladys.noh.medina@gmail.com>
2021-12-09 20:39:56 -0600

    Updated initial login button selector.

--- frontend/pom/components/navbar.js
+++ frontend/pom/components/navbar.js
@@ -2,9 +2,9 @@ import { Selector } from 'testcafe';
 
 class navbar {
     constructor() {
-        this.logInOption = Selector('ul>li>a').nth(4)
+        this.logInOption = Selector('ul>li>a').withAttribute('href', /login/u)
         this.avatarButton = Selector('.top_bar_btn.settings_btn')
-        this.logOutOption = Selector('button .user_menu_label').nth(1)
+        this.logOutOption = Selector('.user_menu button .user_menu_label').nth(1)
     }
 }
 
