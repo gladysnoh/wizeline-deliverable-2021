53cf82bbadb7afb44fa7ba35dd6ea7074118760f
Gladys Noh <gladys.noh.medina@gmail.com>
2021-12-09 21:32:27 -0600

    Added expect step to validate the project color.

--- frontend/pom/pages/project-page.js
+++ frontend/pom/pages/project-page.js
@@ -2,21 +2,31 @@ import { Selector } from 'testcafe';
 
 class ProjectPage {
 
-    RED_COLOR_VALUE = '31';
+    DROPDOWN_RED_COLOR_VALUE = '31';
+    RED_COLOR_STYLE_VALUE = 'color: rgb(219, 64, 53);';
 
     constructor() {
         this.nameProjectInput = Selector('#edit_project_modal_field_name')
         this.colorProjecDropdownMenu = Selector('.color_dropdown_toggle.form_field_control')
         this.redColorProjectOption = Selector('.color_dropdown_select ul > li')
-            .withAttribute('data-value', this.RED_COLOR_VALUE)
+            .withAttribute('data-value', this.DROPDOWN_RED_COLOR_VALUE)
         this.addFavoritesCheckbox = Selector('.reactist_switch')
             .child('input').withAttribute('name', 'is_favorite')
         this.addProjectButton = Selector('footer > button').withExactText('Add')
     }
 
-    getProjectNameSelectorFromList(projectName) {
+    getFavoriteListSelector() {
         return Selector('ul').withAttribute('aria-label', 'Favorites')
-            .child('li').find('span').withExactText(projectName);
+            .child('li')
+    }
+
+    getFavoriteProjectSelectorByProjectName(projectName) {
+        return this.getFavoriteListSelector().find(`a[aria-label*="${projectName}"]`)
+    }
+
+    getProjectIconRedColorByProjectName(projectName) {
+       return this.getFavoriteProjectSelectorByProjectName(projectName).find('svg.project_icon')
+            .withAttribute('style', this.RED_COLOR_STYLE_VALUE)
     }
 }
 
--- frontend/pom/tests/project.test.js
+++ frontend/pom/tests/project.test.js
@@ -9,7 +9,7 @@ fixture('Project feature test')
         await t.useRole(STANDARD_USER)
     })
 
-test.meta('type', 'smoke')('6. As a user I should be able to create a new project', async t => {
+test.meta('type', 'smoke')('6. As a user I should be able to create a new favorite project with red color', async t => {
     await t.click(sidebar.addProjectButton)
     await t.typeText(projectPage.nameProjectInput, PROJECT_DATA.NAME, { paste: true })
     await t.click(projectPage.colorProjecDropdownMenu)
@@ -17,5 +17,8 @@ test.meta('type', 'smoke')('6. As a user I should be able to create a new projec
     await t.click(projectPage.addFavoritesCheckbox)
     await t.click(projectPage.addProjectButton)
     await t.wait(WAIT_TIME.TWO_SECONDS)
-    await t.expect(projectPage.getProjectNameSelectorFromList(PROJECT_DATA.NAME).exists).ok()
+    // Verifies that the added project is on favorite section and saved with the provided name
+    await t.expect(projectPage.getFavoriteProjectSelectorByProjectName(PROJECT_DATA.NAME).exists).ok()
+    // Then validates that the project color is red.
+    await t.expect(projectPage.getProjectIconRedColorByProjectName(PROJECT_DATA.NAME).exists).ok()
 })
